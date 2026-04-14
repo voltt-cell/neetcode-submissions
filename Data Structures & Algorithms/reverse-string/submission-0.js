7e2f13665f7552b7426cc8ec2afad11298114768
Loveleet Verma <58814550+voltt-cell@users.noreply.github.com>
2026-04-14 08:11:42 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s) {
+
+        for (let i=0;i<s.length/2;i++)
+        {
+            let temp=s[i];
+            s[i]=s[s.length-i-1];
+            s[s.length-i-1]=temp
+        }
+
+        return s
+        
+    }
+}
